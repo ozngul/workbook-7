90575e816fdf26f4a91f1c9c12f595a6c8068e6b
Ozan GUL <ozan.gul@outlook.com.tr>
2025-06-02 11:32:48 -0400

    MySQL Exercise

---
+++ EXERCISE 1.sql
@@ -0,0 +1,42 @@
+-- What categories of products does Northwind Traders sell?
+-- Beverages, Condiments, Confections,Dairy Products,Grains/Cereals,Meat/Poultry,Produce,Seafood
+
+-- 2. What is the name of a product that sounds interesting to you?
+-- NuNuCa Nu-Nougat-Creme
+
+-- 3. What are some of the countries in which we have customers?
+-- USA, GERMANY, MEXİCO, CANADA, SPAİN, İTALY, BELGİUM
+
+-- 5. What date was our last order placed?
+-- July 4, 1996
+
+
+-- 6. How many customers do we currently have?
+
+-- 7. Do we have any suppliers in South America? Europe? Africa?
+-- Europe Germany, Spain, Netherland, France
+-- SouthAmerica Brazil, Argentina
+
+-- 8. What is our most expensive product?
+-- Côte de Blaye — $263.50
+
+-- 9. What is our least expensive product?
+-- Geitost
+
+
+
+-- We have a customer named "The Big Cheese" if I wanted to get details about their last order from us how would you find that information?
+	-- a. What tables would you use?
+    -- Customers, Orders, Order Details, Products
+
+	-- b. How would you link the data in the tables you are using
+    
+	-- c. What information might I want to know about their order?
+		-- i. Order Date?
+        -- May 5, 1998
+		-- ii. Sales Total?
+        -- $263.50
+		-- iii. Product Name?
+        -- Côte de Blaye
+		-- iv. Is there other information that you might find interesting?
+        -- Quantitiy ordered
